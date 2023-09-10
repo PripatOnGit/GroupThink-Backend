02235a9f61f2bcee5278742828d65eeb8458c8df
PripatOnGit <patil.priyu26@gmail.com>
2023-09-10 21:33:08 +0530

    few more endpoints

--- db.py
+++ db.py
@@ -1,14 +1,23 @@
 import json
 
 
-def read() -> list:
+def read_all_rows() -> list:
     with open('data.json', 'r') as f:
-        data_lst = json.load(f)
+        data_lst = json.loads(f.read())
         return data_lst
 
 
-def write(data_row):
-    data_lst = read()
+def write_row(data_row):
+    data_lst = read_all_rows()
     data_lst.append(data_row)
     with open('data.json', 'w') as json_file:
-        json.dump(data_lst, json_file)
+        json.dump(data_lst, json_file, indent=4)
+
+
+def update_row(data_row):
+    data_lst = read_all_rows()
+    for row in data_lst:
+        if data_row["username"] == data_row["username"]:
+            row.update(data_row)
+    with open('data.json', 'w') as json_file:
+        json.dump(data_lst, json_file, indent=4)
