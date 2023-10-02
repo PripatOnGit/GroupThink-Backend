4077c04e6eead98b20e2be86a79424d4a33b7ff5
PripatOnGit <patil.priyu26@gmail.com>
2023-10-02 17:37:51 +0530

    adding pg connection

--- app.py
+++ app.py
@@ -1,154 +1,39 @@
+import pg
+
 from flask import Flask
 from flask import request, jsonify
 from flask_cors import CORS
-import hashlib
-import db
-from datetime import datetime
 
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"*": {"origins": "*"}})
 
 
+@app.route("/")
+def index():
+    return "<p>Default Response</p>"
+
+
 @app.route("/api/auth/signup", methods= ['POST'])
 def signup():    
     data = request.get_json()  
-    data["joinedAt"] = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
-    db.write_row(data)
-    return jsonify("Success !!"),200
+    success = pg.create_user(data)
+    if success:
+        return jsonify("Success !!"), 200
+    return jsonify("Failed to create user"), 500
 
 
 @app.route("/api/user/checkUsernameAvailability", methods= ['GET'])
 def checkUserAvailability():
     username = request.args.get("username")
     print("checking username =>", username)
-    data_lst = db.read_all_rows()    
-    for row in data_lst:
-        if row["username"] == username:            
-            return jsonify(False), 200
-    return jsonify(True), 200
+    user_not_found = pg.check_user_availability("username", username)    
+    return jsonify(user_not_found), 200
 
 
 @app.route("/api/user/checkEmailAvailability", methods= ['GET'])
 def checkEmailAvailability():
     email = request.args.get("email")
     print("checking email =>", email)
-    data_lst = db.read_all_rows()    
-    for row in data_lst:
-        if row["email"] == email:            
-            return jsonify(False), 200
-    return jsonify(True), 200
-
-
-@app.route("/api/auth/signin", methods= ['POST'])
-def login():    
-    data = request.get_json()    
-    data_lst = db.read_all_rows()    
-    for row in data_lst:
-        if (row["username"] == data["usernameOrEmail"] or row["email"] == data["usernameOrEmail"]) and row["password"] == data["password"]:            
-            accessToken = hashlib.md5("{0}__{1}".format(row["username"], row["password"]).encode()).hexdigest()
-            return jsonify({"accessToken": accessToken}), 200
-    return jsonify(False), 200
-
-
-@app.route("/api/users/<username>", methods= ['GET'])
-def user_by_name(username):        
-    authorization_header = request.headers.get("Authorization")
-    if not authorization_header:
-        return jsonify("Unauthorized Access"), 401
-    
-    data_lst = db.read_all_rows()        
-    for row in data_lst:
-        accessToken = hashlib.md5("{0}__{1}".format(row["username"], row["password"]).encode()).hexdigest()        
-        if accessToken == authorization_header.split()[1]:            
-            return jsonify({
-                "name": row["name"],
-                "username": row["username"],
-                "joinedAt": row["joinedAt"],
-                "pollCount": len(row["polls"]),
-                "voteCount": 0
-            }), 200
-    return jsonify(False), 200
-
-
-@app.route("/api/user/me", methods= ['GET'])
-def user_me():        
-    authorization_header = request.headers.get("Authorization")
-    if not authorization_header:
-        return jsonify("Unauthorized Access"), 401
-    
-    data_lst = db.read_all_rows()        
-    for row in data_lst:
-        accessToken = hashlib.md5("{0}__{1}".format(row["username"], row["password"]).encode()).hexdigest()        
-        if accessToken == authorization_header.split()[1]:            
-            return jsonify({"username": row["username"]}), 200
-    return jsonify(False), 200
-
-
-@app.route("/api/polls", methods= ['GET'])
-def get_polls():        
-    authorization_header = request.headers.get("Authorization")
-    if not authorization_header:
-        return jsonify("Unauthorized Access"), 401
-    
-    data_lst = db.read_all_rows()        
-    for row in data_lst:
-        accessToken = hashlib.md5("{0}__{1}".format(row["username"], row["password"]).encode()).hexdigest()        
-        if accessToken == authorization_header.split()[1]:         
-            for poll in row["polls"]:
-                poll["createdBy"] = {"username": row["username"], "name": row["name"]}
-            return jsonify({
-                "content": row["polls"],
-                "page": 1,
-                "size": 1,
-                "totalPages": 1,
-                "totalElemets": 1,
-                "last": 1                
-            }), 200
-    return jsonify(False), 200
-
-
-@app.route("/api/users/<username>/polls", methods= ['GET'])
-def get_polls_by_user(username):        
-    authorization_header = request.headers.get("Authorization")
-    if not authorization_header:
-        return jsonify("Unauthorized Access"), 401
-    
-    data_lst = db.read_all_rows()        
-    for row in data_lst:
-        accessToken = hashlib.md5("{0}__{1}".format(row["username"], row["password"]).encode()).hexdigest()        
-        if accessToken == authorization_header.split()[1]:         
-            for poll in row["polls"]:
-                poll["createdBy"] = {"username": row["username"], "name": row["name"]}
-            return jsonify({
-                "content": row["polls"],
-                "page": 1,
-                "size": 1,
-                "totalPages": 1,
-                "totalElemets": 1,
-                "last": 1                
-            }), 200
-    return jsonify(False), 200
-
-
-@app.route("/api/polls", methods= ['POST'])
-def submit_polls():        
-    authorization_header = request.headers.get("Authorization")
-    if not authorization_header:
-        return jsonify("Unauthorized Access"), 401
-    
-    poll = request.get_json()
-    poll["creationDateTime"] = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
-    data_lst = db.read_all_rows()        
-    for row in data_lst:
-        accessToken = hashlib.md5("{0}__{1}".format(row["username"], row["password"]).encode()).hexdigest()        
-        if accessToken == authorization_header.split()[1]:
-            row.get("polls", []).append(poll)
-            db.update_row(row)
-            return jsonify(True), 200
-    return jsonify(False), 200
-
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+    user_not_found = pg.check_user_availability("email", email)    
+    return jsonify(user_not_found), 200
---
+++ pg.py
@@ -0,0 +1,61 @@
+import psycopg2
+import hashlib
+from datetime import datetime
+
+
+def connect_to_pg():
+    conn_string = "host='localhost' dbname='polls_db' user='postgres' password='root'"
+    conn = psycopg2.connect(conn_string)
+    print("Connection Successful!")
+    return conn
+
+
+def execute_select_query(query, params=None):
+    conn = connect_to_pg()
+    cursor = conn.cursor()
+    print(f"Executing {query} with Params {params or []}")
+    cursor.execute(query, params)    
+    results = cursor.fetchall()
+    print(f"Affected {cursor.rowcount} rows")    
+    conn.close()
+    return results
+
+
+def execute_update_query(query, params=None):
+    conn = connect_to_pg()
+    cursor = conn.cursor()
+    print(f"Executing {query} with Params {params or []}")
+    cursor.execute(query, params)    
+    conn.commit()
+    row_count = cursor.rowcount    
+    conn.close()
+    print(f"Affected {row_count} rows")    
+    return row_count
+
+
+def create_user(data):
+    data["joinedAt"] = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
+    data['password'] = hashlib.md5(data['password'].encode()).hexdigest()
+    query = "INSERT INTO USERS (Name, Email, Username, Password, JoinedAt) VALUES (%s,%s,%s,%s,%s)"
+    res = execute_update_query(query, (data['name'], data['email'], data['username'], data['password'], data['joinedAt']))        
+    return res == 1
+        
+    
+def check_user_availability(field, value):
+    query = f"SELECT 1 FROM USERS WHERE {field} = %s"
+    user_found = execute_select_query(query, (value,))    
+    return None if not user_found else False
+
+
+def read_all_users() -> list:
+    pass
+
+
+def read_all_polls() -> list:
+    conn = connect_to_pg()
+    cursor = conn.cursor()
+    records = cursor.execute("")
+
+
+if __name__ == "__main__":
+    connect_to_pg()
